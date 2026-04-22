6a865f2420380a219a34b1bc12e8b7cffba7cf20
Shambhu <148757019+Shambhu902@users.noreply.github.com>
2026-04-22 22:04:10 +0530

    mplement search functionality; add SearchBar and RelatedProducts components; update ShopContext for search state management; modify Navbar to trigger search visibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,17 @@ import Orders from './pages/Orders'
 import Login from './pages/Login'
 import Cart from './pages/Cart'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
       <Navbar />
+      <SearchBar />
       <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
@@ -26,6 +32,7 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/cart" element={<Cart />} />
         </Routes>
+        <Footer />
 
     </div>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false)
+    const {setShowSearch} = useContext(ShopContext);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <Link to="/"><img src={assets.logo} className='w-36' alt="Logo" /></Link>
@@ -27,7 +28,8 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-6'>
-        <img src={assets.cart_icon} className='w-5 cursor-pointer' alt="Cart" />
+        <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="Search" />
+      
 
         <div className='group relative'>
             <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="User" />
